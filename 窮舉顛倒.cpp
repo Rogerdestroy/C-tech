e4f8358e21bbaa3ee58ae2a053406e2a2868416c
Rogerdestroy <roge000000mymail@gmail.com>
2022-04-07 09:19:40 +0800

    窮舉題目

---
+++ 窮舉顛倒.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    int a[10000];
+    while(cin>>n){
+        for(int i=0;i<n;i++) a[i]=n-i;
+        do{
+            for(int i=0;i<n;i++)cout<<a[i];
+            cout<<endl;
+        }while(prev_permutation(a,a+n));
+    }
+}
