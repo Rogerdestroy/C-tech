79999b1d56485df9fa13c22f2c1cb5b36166b142
Rogerdestroy <roge000000mymail@gmail.com>
2022-04-08 09:44:22 +0800

    initial commit

--- C++範例程式/ch01/ch1-2a(1).cpp
+++ C++範例程式/ch01/ch1-2a(1).cpp
@@ -1,11 +1,21 @@
+/*
+output num average in c++
+*/
 #include <iostream>
 using namespace std;
-int main(int argc, char *argv[])
-{
-    float Average=0;
-    int  score1=60,score2=70,score3=80,score4=85,score5=90;
-    Average = (float)(score1+score2+score3+score4+score5)/5 ;
-    cout<<"¥­§¡¦¨ÁZ¡G\n"<<Average;
-    system("PAUSE");	
-    return 0;
+
+int main() {
+	int num;
+	int sum = 0;
+	int count = 0;
+	cout << "Enter a number: ";
+	cin >> num;
+	while (num != -1) {
+		sum += num;
+		count++;
+		cout << "Enter a number: ";
+		cin >> num;
+	}
+	cout << "The average is " << sum / count << endl;
+	return 0;
 }
---
+++ C++範例程式/test coplite/test coplite/CommonIncludes.h
@@ -0,0 +1,14 @@
+﻿// CommonIncludes.h
+
+#pragma once
+
+#include "Resource.h"
+#include "..\test copliteUI\Resource.h"
+#include "Guids.h"
+#include "..\test copliteUI\CommandIds.h"
+
+#include "test coplite.h"
+
+#include "Package.h"
+
+
