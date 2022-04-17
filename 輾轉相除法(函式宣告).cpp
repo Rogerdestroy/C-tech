5e1b235764e65a1d2f9adb128d78adaa8fb728ff
Rogerdestroy <82896473+Rogerdestroy@users.noreply.github.com>
2022-04-17 15:44:47 +0800

    Update 輾轉相除法(函式宣告).cpp

--- 輾轉相除法(函式宣告).cpp
+++ 輾轉相除法(函式宣告).cpp
@@ -15,3 +15,21 @@ int main()
     return 0;
 }
 
+//----------------------------------------------
+
+#include <iostream>
+#include <type_traits>
+
+template<typename T, std::enable_if_t<std::is_integral<T>::value, T> a, std::enable_if_t<std::is_integral<T>::value, T> b>
+struct HCF{
+public:
+    static const T value=HCF<T, (a>b? b: a), (a>b? a%b: b%a)>::value;
+};
+template<typename T, std::enable_if_t<std::is_integral<T>::value, T> a>
+struct HCF<T, a, 0>{
+public:
+    static const T value=a;
+};
+int main(){
+    std::wcout<<HCF<int, 12, 64>::value<<std::endl;//12 64 Output: 4
+}
